8da58fa3495ba36dffd88d94411add6c841e3dcd
7thCoder <nuibima@gmail.com>
2023-12-07 19:09:26 +0100

    added project files

---
+++ Socket_programming/src/main/java/com/mycompany/socket_programming/Socket_programming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.socket_programming;
+
+/**
+ *
+ * @author SHAKO
+ */
+public class Socket_programming {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
